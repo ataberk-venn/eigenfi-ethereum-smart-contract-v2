d38831dc0abc3c2629960f0b5ecaa461b38ceb73
chingunee <chingunee.dev@gmail.com>
2024-08-14 11:51:18 +0800

    updated contract address

--- ignition/modules/EigenFiPoolModule.js
+++ ignition/modules/EigenFiPoolModule.js
@@ -1,8 +1,8 @@
 const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 
-const _signer = "0x900a7A576E37d1dc9B6B8D31BDdd78470052f8F7";
+const _signer = "0xCa28eaA4fFF145c26074F3EA08b657B288401E33";
 const _weth = "0x7b79995e5f793A07Bc00c21412e50Ecae098E7f9";
-const _tokensAllowed = ["0x2a8da3AFB77096530146FFc544d5714C63C3CE4B"];
+const _tokensAllowed = ["0xeBB643800FF0bfCf4D4b3446B78654f924d1907F"];
 
 module.exports = buildModule("EigenFiPoolModule", (m) => {
   const eigenFiPool = m.contract("EigenFiPool", [
